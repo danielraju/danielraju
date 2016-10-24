abdafdf0cf3c3f33cdf746a53cc2b72a3fea8802
danielraju <daniel7dj@gmail.com>
2016-10-24 10:09:20 +0530

    Update and rename .gitignore to number.c

---
+++ number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int no;
+printf("enter the number:");
+scanf("%d",&no);
+if(no=>0)
+{
+printf("the no is positive",no);
+else
+printf("the no is negative",no);
+}
+return 0;
+}
+
